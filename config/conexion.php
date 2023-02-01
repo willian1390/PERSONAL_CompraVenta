ebbcfbad77c08d0df522b5ce89c80fe8c15560ad
Jose <jhernandezr30@miumg.edu.gt>
2023-02-01 10:01:43 -0600

    modificacion de conexion

--- config/conexion.php
+++ config/conexion.php
@@ -2,11 +2,12 @@
   class Conectar{
     protected $dbh;
 
-    protected function conexion(){
+    protected function Conexion(){
       try {
         $conectar = $this->dbh=new PDO("sqlsrv:Server=localhost;Database=CompraVenta","sa","admin1");
       } catch (Exception $e) {
         print "Error Conexion BD". $e->getMessage() . "<br/>";
+        die();
       }
     }
   }
